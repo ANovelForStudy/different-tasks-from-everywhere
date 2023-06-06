366dc7447abc73960f204de6eabefef6eb218eba
Roman Likhachev <anovelforstudy@gmail.com>
2023-06-06 05:07:14 +0300

    Added two 'Easy' tasks (#1502, #217-Python) from 'Array' in LeetCode

---
+++ LeetCode/Array/Easy/1502_Can_Make_Arithmetic_Progression_From_Sequence/Python/main.py
@@ -0,0 +1,12 @@
+
+from typing import List
+
+
+class Solution:
+    def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
+        arr.sort()
+        k = arr[1] - arr[0]
+        for i in range(1, len(arr)):
+            if arr[i] - arr[i-1] != k:
+                return False
+        return True
---
+++ LeetCode/Array/Easy/217_Contains_Duplicate/Python/tests.py
@@ -0,0 +1,31 @@
+
+from main import Solution
+
+
+def base_test(tests=None, expects=None, is_verbose=True):
+
+    _ = Solution()
+
+    if tests is None:
+        tests = [
+            [1, 2, 3, 1],
+            [1, 2, 3, 4],
+        ]
+
+    if expects is None:
+        expects = [
+            True,
+            False,
+        ]
+
+    for test, expected, counter in zip(tests, expects, range(1, len(tests)+1)):
+        result = _.containsDuplicate(test)
+        assert result == expected, f"Test #{counter} failed.\n"\
+            f"\tExpected: {expected}\n"\
+            f"\tReceived: {result}\n"
+        print("Test #{} passed.{}".format(
+            counter, f"\n\tTest case: {test}" if is_verbose else ""))
+
+
+if __name__ == "__main__":
+    base_test()
